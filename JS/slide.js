b73de24a2d065617bcdfe992221f00e803197925
saripagurek <saripvm@gmail.com>
2023-08-19 12:35:37 -0400

    update logo

---
+++ JS/slide.js
@@ -0,0 +1,17 @@
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    const square = entry.target.querySelector('.square');
+
+    if (entry.isIntersecting) {
+      square.classList.add('square-animation');
+      square.classList.remove('square-hide');
+    return; // if we added the class, exit the function
+    }
+
+    // We're not intersecting, so remove the class!
+    square.classList.remove('square-animation');
+    quare.classList.add('square-hide');
+  });
+});
+
+observer.observe(document.querySelector('.square-wrapper'));
